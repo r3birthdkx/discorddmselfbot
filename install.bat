56dca31b298f4d5ae81f6f5a8451d328ddbb861c
r3birthdkx <95663370+r3birthdkx@users.noreply.github.com>
2021-12-06 22:20:23 -0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,37 @@
+@echo off
+color 4
+title r3birth selfbot installer
+echo.
+echo.
+echo                   welcome to the installer
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause nul
+cls
+color 5
+cls
+call npm install function-bind
+cls
+call npm install discord.js-selfbot-v11
+cls
+call npm install discordjs-lofy
+cls
+call npm install colors
+cls
+call npm install npm install node-fetch@2.6.1
+cls
+call npm install fs
+cls
+color 5
+echo.
+echo.
+echo                   now you can close this tab
+echo.
+echo.
+pause nul
+exit
